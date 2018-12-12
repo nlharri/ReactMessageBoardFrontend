85a2a6324a4ac335d531bde45b2be2a1e6254f38
Harri <nlharri@gmail.com>
2018-12-12 16:12:35 +0000

    initial layout with React + MaterializeCSS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class MessageBoardErrorContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Error Container</h2>
+        <MessageBoardErrorMessage />
+        <MessageBoardErrorMessage />
+        <MessageBoardErrorMessage />
+      </div>
+    );
+  }
+}
+
+class MessageBoardForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Form</h2>
+      </div>
+    );
+  }
+}
+
+class MessageBoardMessageContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Message Container</h2>
+        <MessageBoardMessage />
+        <MessageBoardMessage />
+        <MessageBoardMessage />
+      </div>
+    );
+  }
+}
+
+class MessageBoardMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Message</h3>
+      </div>
+    );
+  }
+}
+
+class MessageBoardErrorMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Error Message</h3>
+      </div>
+    );
+  }
+}
+
+class MessageBoardContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Message Board Container</h1>
+        <MessageBoardErrorContainer />
+        <MessageBoardForm />
+        <MessageBoardMessageContainer />
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <MessageBoardContainer />
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
