dc2be59021ecc9e5ac473d94cb30c08422240dbc
Harri <nlharri@gmail.com>
2018-12-13 11:26:04 +0000

    Enhanced with form submission. Expand not working yet due to MaterializeCSS + React problems

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+var Moment = require('moment');
+
 class MessageBoardErrorContainer extends React.Component {
   render() {
     return (
@@ -15,36 +17,77 @@ class MessageBoardErrorContainer extends React.Component {
 }
 
 class MessageBoardForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nickName: '',
+      messageText: '',
+      timeStampText: '',
+    };
+    this.handleNickNameChange = this.handleNickNameChange.bind(this);
+    this.handleMessageTextChange = this.handleMessageTextChange.bind(this);
+  }
+
+  handleNickNameChange(e) {
+    this.setState({
+      nickName: e.target.value,
+      timeStampText: Moment().format('MMMM Do YYYY, h:mm:ss a'),
+    });
+  }
+  
+  handleMessageTextChange(e) {
+    this.setState({
+      messageText: e.target.value,
+      timeStampText: Moment().format('MMMM Do YYYY, h:mm:ss a'),
+    });
+  }
+  
   render() {
     return (
       <div>
-        <ul class="collapsible white" data-collapsible="expandable">
+        <ul className="collapsible white" data-collapsible="expandable">
           <li>
-            <div class="collapsible-header active waves-effect waves-yellow light-green accent-1">
-              <i class="material-icons">add_comment</i>New Message
+            <div className="collapsible-header active waves-effect waves-yellow light-green accent-1">
+              <i className="material-icons">add_comment</i>New Message
             </div>
-            <div class="collapsible-body">
-              <div class="row">
-                <form class="col s12">
-                  <div class="row">
-                    <div class="input-field col s12 ">
+            <div className="collapsible-body">
+              <div className="row">
+                <form onSubmit={(e) => {e.preventDefault(); this.props.onFormSubmit(this.state)}} className="col s12">
+                  <div className="row">
+                    <div className="input-field col s12 ">
                       <p>
-                        <input id="nickname" placeholder="Nickname" type="text" class="validate" length="30" />
-                        <span class="helper-text">(alphanumerical characters)</span>
+                        <input
+                          onChange={e => this.handleNickNameChange(e)}
+                          id="nickname" 
+                          placeholder="Nickname" 
+                          type="text" 
+                          className="validate" 
+                          length="30" />
+                        <span className="helper-text">(alphanumerical characters)</span>
                       </p>
                     </div>
-                    <div class="input-field col s12 ">
+                    <div className="input-field col s12 ">
                       <p>
-                        <textarea id="message" placeholder="Message" name="message" form="newmessage" class="materialize-textarea" length="300"></textarea>
-                        <span class="helper-text">(alphanumerical characters)</span>
+                        <textarea
+                          onChange={e => this.handleMessageTextChange(e)}
+                          id="message" 
+                          placeholder="Message" 
+                          name="message" 
+                          className="materialize-textarea" 
+                          length="300"></textarea>
+                        <span className="helper-text">(alphanumerical characters)</span>
                       </p>
                     </div>
                   </div>
-                  <div class="row right">
-                    <div class="col s12">
+                  <div className="row right">
+                    <div className="col s12">
                       <p>
-                        <button class="btn black-text waves-effect waves-yellow light-green accent-1" type="submit" name="action">Submit
-                          <i class="material-icons right">create</i>
+                        <button 
+                          className="btn black-text waves-effect waves-yellow light-green accent-1" 
+                          type="submit" 
+                          name="action">
+                          Submit
+                          <i className="material-icons right">create</i>
                         </button>
                       </p>
                     </div>
@@ -61,13 +104,18 @@ class MessageBoardForm extends React.Component {
 
 class MessageBoardMessageContainer extends React.Component {
   render() {
-    return (
-      <div>
-        <MessageBoardMessage />
-        <MessageBoardMessage />
-        <MessageBoardMessage />
-      </div>
-    );
+    var messages = [];
+    for (var i = 0; i < this.props.messageBoardMessages.length; i++) {
+      messages.unshift(
+        <MessageBoardMessage 
+          key={i} 
+          nickName={this.props.messageBoardMessages[i].nickName} 
+          messageText={this.props.messageBoardMessages[i].messageText} 
+          timeStampText={this.props.messageBoardMessages[i].timeStampText}
+        />
+      );
+    }
+    return <div>{messages}</div>;
   }
 }
 
@@ -75,13 +123,13 @@ class MessageBoardMessage extends React.Component {
   render() {
     return (
       <div>
-        <ul class="collapsible white" data-collapsible="expandable">
+        <ul className="collapsible white" data-collapsible="expandable">
           <li>
-            <div class="collapsible-header active waves-effect waves-yellow light-green accent-1">
-              <i class="material-icons">message</i>Harri says (December 11th 2018, 2:23:23 pm)
+            <div className="collapsible-header active waves-effect waves-yellow light-green accent-1">
+              <i className="material-icons">message</i>{this.props.nickName} says ({this.props.timeStampText}):
             </div>
-            <div class="collapsible-body">
-              <p>"Hello World!"</p>
+            <div className="collapsible-body">
+              <i></i>{this.props.messageText}
             </div>
           </li>
         </ul>
@@ -94,12 +142,12 @@ class MessageBoardErrorMessage extends React.Component {
   render() {
     return (
       <div>
-        <ul class="collapsible white" data-collapsible="expandable">
+        <ul className="collapsible white" data-collapsible="expandable">
           <li>
-            <div class="collapsible-header active waves-effect waves-yellow red accent-3">
-              <i class="material-icons">error</i>Error
+            <div className="collapsible-header active waves-effect waves-yellow red accent-3">
+              <i className="material-icons">error</i>Error
             </div>
-            <div class="collapsible-body">
+            <div className="collapsible-body">
               <p>"Error!"</p>
             </div>
           </li>
@@ -110,23 +158,52 @@ class MessageBoardErrorMessage extends React.Component {
 }
 
 class MessageBoardContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessages: [],
+      messageBoardMessages: [],
+    };
+  }
+
   render() {
     return (
       <div className="container">
-        <div class="col s12">
+        <div className="col s12">
           <h4>MESSAGE<b>BOARD</b>APP</h4>
         </div>
-        <div class="col s12">
-          <h6 class="right-align"><b>&copy; 2019 NLHARRI</b></h6>
+        <div className="col s12">
+          <h6 className="right-align"><b>&copy; 2019 NLHARRI</b></h6>
         </div>
         <div>
-          <MessageBoardErrorContainer />
-          <MessageBoardForm />
-          <MessageBoardMessageContainer />
+          <MessageBoardErrorContainer errorMessages={this.state.errorMessages}/>
+          <MessageBoardForm onFormSubmit={(formData) => this.handleSubmit(formData)}/>
+          <MessageBoardMessageContainer messageBoardMessages={this.state.messageBoardMessages}/>
         </div>
       </div>
     );
   }
+  
+  // formData is an array of messages
+  handleSubmit(formData) {
+    const messageBoardMessagesCopy = this.state.messageBoardMessages.slice(); 
+    var errorMessagesNew = this.validate(formData);
+    if (errorMessagesNew.length === 0) {
+      messageBoardMessagesCopy.unshift(formData);
+    }
+    this.setState({
+      errorMessages: errorMessagesNew,
+      messageBoardMessages: messageBoardMessagesCopy,
+    });
+    console.log("handleSubmit was called with: " + formData.nickName + " " + formData.messageText + " " + formData.timeStampText);
+  }
+  
+  // validate form data
+  validate(formData) {
+    return [];
+  }
+  
+  
 }
 
 function App() {
